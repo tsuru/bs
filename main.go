4a35f0af37e7e767647b02fb0124d3dc07913cd9
Francisco Souza <fss@corp.globo.com>
2015-05-22 18:45:37 -0300

    main: starting the project
    The first function will just load the configuration from the environment
    and store in a global object.

---
+++ main.go
@@ -0,0 +1,24 @@
+// Copyright 2015 bs authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"os"
+)
+
+var config struct {
+	DockerEndpoint string
+	TsuruEndpoint  string
+	SentinelEnvVar string
+}
+
+func loadConfig() {
+	config.DockerEndpoint = os.Getenv("DOCKER_ENDPOINT")
+	config.TsuruEndpoint = os.Getenv("TSURU_ENDPOINT")
+	config.SentinelEnvVar = os.Getenv("TSURU_SENTINEL_ENV_VAR")
+}
+
+func main() {
+}
---
+++ main_test.go
@@ -0,0 +1,30 @@
+// Copyright 2015 bs authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"os"
+	"testing"
+
+	"gopkg.in/check.v1"
+)
+
+var _ = check.Suite(S{})
+
+func Test(t *testing.T) {
+	check.TestingT(t)
+}
+
+type S struct{}
+
+func (S) TestLoadConfig(c *check.C) {
+	os.Setenv("DOCKER_ENDPOINT", "http://192.168.50.4:2375")
+	os.Setenv("TSURU_ENDPOINT", "http://192.168.50.4:8080")
+	os.Setenv("TSURU_SENTINEL_ENV_VAR", "TSURU_APP_NAME")
+	loadConfig()
+	c.Check(config.DockerEndpoint, check.Equals, "http://192.168.50.4:2375")
+	c.Check(config.TsuruEndpoint, check.Equals, "http://192.168.50.4:8080")
+	c.Check(config.SentinelEnvVar, check.Equals, "TSURU_APP_NAME")
+}
