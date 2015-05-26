61a6d66dc1727d3dba7908c26c5ef99a951725aa
Francisco Souza <fss@corp.globo.com>
2015-05-26 15:31:58 -0300

    main: actually running something in the function main
    Calling the status reporter, and ensure that we don't interrupt it when
    certain signals arrive.

--- main.go
+++ main.go
@@ -4,13 +4,23 @@
 
 package main
 
-import "os"
+import (
+	"log"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
+)
+
+const defaultInterval = 60
 
 var config struct {
 	DockerEndpoint string
 	TsuruEndpoint  string
 	TsuruToken     string
 	SentinelEnvVar string
+	StatusInterval time.Duration
 }
 
 func loadConfig() {
@@ -18,7 +28,47 @@ func loadConfig() {
 	config.TsuruEndpoint = os.Getenv("TSURU_ENDPOINT")
 	config.TsuruToken = os.Getenv("TSURU_TOKEN")
 	config.SentinelEnvVar = os.Getenv("TSURU_SENTINEL_ENV_VAR") + "="
+	statusInterval := os.Getenv("STATUS_INTERVAL")
+	parsedInterval, err := strconv.Atoi(statusInterval)
+	if err != nil {
+		log.Printf("[WARNING] invalid interval %q. Using the default value of %d seconds", statusInterval, defaultInterval)
+		parsedInterval = defaultInterval
+	}
+	config.StatusInterval = time.Duration(parsedInterval) * time.Second
+}
+
+func statusReporter() (chan<- struct{}, <-chan struct{}) {
+	abort := make(chan struct{})
+	exit := make(chan struct{})
+	go func(abort <-chan struct{}) {
+		for {
+			select {
+			case <-abort:
+				close(exit)
+				return
+			case <-time.After(config.StatusInterval):
+				collectStatus()
+			}
+		}
+	}(abort)
+	return abort, exit
+}
+
+func startSignalHandler(callback func(os.Signal), signals ...os.Signal) {
+	sigChan := make(chan os.Signal, 4)
+	go func() {
+		if signal, ok := <-sigChan; ok {
+			callback(signal)
+		}
+	}()
+	signal.Notify(sigChan, signals...)
 }
 
 func main() {
+	loadConfig()
+	abortReporter, reporterEnded := statusReporter()
+	startSignalHandler(func(signal os.Signal) {
+		close(abortReporter)
+	}, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
+	<-reporterEnded
 }
--- main_test.go
+++ main_test.go
@@ -5,8 +5,11 @@
 package main
 
 import (
+	"bytes"
+	"log"
 	"os"
 	"testing"
+	"time"
 
 	"gopkg.in/check.v1"
 )
@@ -24,9 +27,29 @@ func (S) TestLoadConfig(c *check.C) {
 	os.Setenv("TSURU_ENDPOINT", "http://192.168.50.4:8080")
 	os.Setenv("TSURU_SENTINEL_ENV_VAR", "TSURU_APP_NAME")
 	os.Setenv("TSURU_TOKEN", "sometoken")
+	os.Setenv("STATUS_INTERVAL", "45")
 	loadConfig()
 	c.Check(config.DockerEndpoint, check.Equals, "http://192.168.50.4:2375")
 	c.Check(config.TsuruEndpoint, check.Equals, "http://192.168.50.4:8080")
 	c.Check(config.TsuruToken, check.Equals, "sometoken")
 	c.Check(config.SentinelEnvVar, check.Equals, "TSURU_APP_NAME=")
+	c.Check(config.StatusInterval, check.Equals, time.Duration(45e9))
+}
+
+func (S) TestLoadConfigInvalidDuration(c *check.C) {
+	var buf bytes.Buffer
+	log.SetOutput(&buf)
+	defer log.SetOutput(os.Stderr)
+	os.Setenv("DOCKER_ENDPOINT", "http://192.168.50.4:2375")
+	os.Setenv("TSURU_ENDPOINT", "http://192.168.50.4:8080")
+	os.Setenv("TSURU_SENTINEL_ENV_VAR", "TSURU_APP_NAME")
+	os.Setenv("TSURU_TOKEN", "sometoken")
+	os.Setenv("STATUS_INTERVAL", "four")
+	loadConfig()
+	c.Check(config.DockerEndpoint, check.Equals, "http://192.168.50.4:2375")
+	c.Check(config.TsuruEndpoint, check.Equals, "http://192.168.50.4:8080")
+	c.Check(config.TsuruToken, check.Equals, "sometoken")
+	c.Check(config.SentinelEnvVar, check.Equals, "TSURU_APP_NAME=")
+	c.Check(config.StatusInterval, check.Equals, time.Duration(60e9))
+	c.Assert(buf.String(), check.Matches, `(?m).*\[WARNING\] invalid interval "four"\. Using the default value of 60 seconds$`)
 }
