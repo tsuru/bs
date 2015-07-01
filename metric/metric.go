1fd1d2fb720ebbbc146a13b3b6c178753107e89f
Andrews Medina <andrewsmedina@gmail.com>
2015-07-01 16:48:26 -0300

    add initial structure to get metrics from containers.

---
+++ metric/metric.go
@@ -0,0 +1,102 @@
+// Copyright 2015 bs authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package metric
+
+import (
+	"log"
+	"sync"
+
+	"github.com/fsouza/go-dockerclient"
+)
+
+var DockerEndpoint string
+
+type statter interface {
+	Send(key, value string) error
+}
+
+type statsd struct{}
+
+func (s *statsd) Send(key, value string) error {
+	return nil
+}
+
+type logStash struct{}
+
+func (s *logStash) Send(key, value string) error {
+	return nil
+}
+
+func getStatter(container *docker.Container) statter {
+	return &logStash{}
+}
+
+func reportMetrics() {
+	client, err := docker.NewClient(DockerEndpoint)
+	if err != nil {
+		log.Printf("[ERROR] cannot create dockerclient instance: %s", err)
+		return
+	}
+	containers, err := client.ListContainers(docker.ListContainersOptions{})
+	if err != nil {
+		log.Printf("[ERROR] failed to list containers in the Docker server at %q: %s", DockerEndpoint, err)
+		return
+	}
+	getMetrics(containers)
+}
+
+func metricsEnabled(container *docker.Container) bool {
+	return false
+}
+
+func getMetrics(containers []docker.APIContainers) {
+	var wg sync.WaitGroup
+	for _, container := range containers {
+		wg.Add(1)
+		go func(c docker.APIContainers) {
+			defer wg.Done()
+			client, err := docker.NewClient(DockerEndpoint)
+			if err != nil {
+				log.Printf("[ERROR] cannot create dockerclient instance: %s", err)
+				return
+			}
+			container, err := client.InspectContainer(c.ID)
+			if err != nil {
+				log.Printf("[ERROR] cannot inspect container %q dockerclient instance: %s", container, err)
+				return
+			}
+			if !metricsEnabled(container) {
+				log.Printf("[INFO] metrics not enabled for container %q. Skipping.", container.ID)
+				return
+			}
+			metrics, err := getMetricFromContainer(container)
+			if err != nil {
+				log.Printf("[ERROR] failed to get metrics for container %q in the Docker server at %q: %s", container, DockerEndpoint, err)
+				return
+			}
+			err = sendMetrics(container, metrics)
+			if err != nil {
+				log.Printf("[ERROR] failed to send metrics for container %q: %s", container, err)
+			}
+		}(container)
+	}
+	wg.Wait()
+}
+
+func getMetricFromContainer(container *docker.Container) (map[string]string, error) {
+	return nil, nil
+}
+
+func sendMetrics(container *docker.Container, metrics map[string]string) error {
+	st := getStatter(container)
+	for key, value := range metrics {
+		err := st.Send(key, value)
+		if err != nil {
+			log.Printf("[ERROR] failed to send metrics for container %q: %s", container, err)
+			return err
+		}
+	}
+	return nil
+}
---
+++ metric/metric_test.go
@@ -0,0 +1,31 @@
+// Copyright 2015 bs authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package metric
+
+import (
+	"testing"
+
+	"github.com/fsouza/go-dockerclient"
+	"gopkg.in/check.v1"
+)
+
+var _ = check.Suite(S{})
+
+func Test(t *testing.T) {
+	check.TestingT(t)
+}
+
+type S struct{}
+
+func (S) TestSendMetrics(c *check.C) {
+	var cont docker.Container
+	err := sendMetrics(&cont, nil)
+	c.Assert(err, check.IsNil)
+}
+
+func (S) TestGetMetrics(c *check.C) {
+	var containers []docker.APIContainers
+	getMetrics(containers)
+}
