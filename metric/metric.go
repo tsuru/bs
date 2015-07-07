4c45177f94278baa087b2dc6a226e425d0564c8a
Andrews Medina <andrewsmedina@gmail.com>
2015-07-07 18:34:18 -0300

    metrics: add getMetricFromContainer.

--- metric/metric.go
+++ metric/metric.go
@@ -6,6 +6,7 @@ package metric
 
 import (
 	"log"
+	"strconv"
 	"strings"
 	"sync"
 
@@ -95,7 +96,49 @@ func getMetrics(containers []docker.APIContainers) {
 }
 
 func getMetricFromContainer(container *docker.Container) (map[string]string, error) {
-	return nil, nil
+	client, err := docker.NewClient(DockerEndpoint)
+	if err != nil {
+		log.Printf("[ERROR] cannot create dockerclient instance: %s", err)
+		return nil, err
+	}
+	statsC := make(chan *docker.Stats)
+	opts := docker.StatsOptions{
+		ID:     container.ID,
+		Stream: false,
+		Stats:  statsC,
+	}
+	go func() {
+		err := client.Stats(opts)
+		if err != nil {
+			log.Printf("[ERROR] cannot get stats for container %q: %s", container, err)
+			return
+		}
+	}()
+	s := <-statsC
+	previousCPU := s.PreCPUStats.CPUUsage.TotalUsage
+	previousSystem := s.PreCPUStats.SystemCPUUsage
+	cpuPercent := calculateCPUPercent(previousCPU, previousSystem, s)
+	memPercent := float64(s.MemoryStats.Usage) / float64(s.MemoryStats.Limit) * 100.0
+	stats := map[string]string{
+		"cpu_max":     strconv.FormatFloat(cpuPercent, 'f', 2, 64),
+		"mem_max":     strconv.FormatUint(s.MemoryStats.Usage, 10),
+		"mem_pct_max": strconv.FormatFloat(memPercent, 'f', 2, 64),
+	}
+	return stats, nil
+}
+
+func calculateCPUPercent(previousCPU, previousSystem uint64, s *docker.Stats) float64 {
+	var (
+		cpuPercent = 0.0
+		// calculate the change for the cpu usage of the container in between readings
+		cpuDelta = float64(s.CPUStats.CPUUsage.TotalUsage - previousCPU)
+		// calculate the change for the entire system between readings
+		systemDelta = float64(s.CPUStats.SystemCPUUsage - previousSystem)
+	)
+	if systemDelta > 0.0 && cpuDelta > 0.0 {
+		cpuPercent = (cpuDelta / systemDelta) * float64(len(s.CPUStats.CPUUsage.PercpuUsage)) * 100.0
+	}
+	return cpuPercent
 }
 
 func sendMetrics(container *docker.Container, metrics map[string]string) error {
--- metric/metric_test.go
+++ metric/metric_test.go
@@ -5,6 +5,8 @@
 package metric
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/fsouza/go-dockerclient"
@@ -66,4 +68,122 @@ func (S) TestGetStatter(c *check.C) {
 	st = getStatter(&cont)
 	c.Assert(st, check.Implements, &s)
 	c.Assert(st, check.FitsTypeOf, &statsd{})
+}
+
+func (S) TestGetMetricFromContainer(c *check.C) {
+	jsonStats := `{
+       "read" : "2015-01-08T22:57:31.547920715Z",
+       "network" : {
+          "rx_dropped" : 0,
+          "rx_bytes" : 648,
+          "rx_errors" : 0,
+          "tx_packets" : 8,
+          "tx_dropped" : 0,
+          "rx_packets" : 8,
+          "tx_errors" : 0,
+          "tx_bytes" : 648
+       },
+       "memory_stats" : {
+          "stats" : {
+             "total_pgmajfault" : 0,
+             "cache" : 0,
+             "mapped_file" : 0,
+             "total_inactive_file" : 0,
+             "pgpgout" : 414,
+             "rss" : 6537216,
+             "total_mapped_file" : 0,
+             "writeback" : 0,
+             "unevictable" : 0,
+             "pgpgin" : 477,
+             "total_unevictable" : 0,
+             "pgmajfault" : 0,
+             "total_rss" : 6537216,
+             "total_rss_huge" : 6291456,
+             "total_writeback" : 0,
+             "total_inactive_anon" : 0,
+             "rss_huge" : 6291456,
+         "hierarchical_memory_limit": 189204833,
+             "total_pgfault" : 964,
+             "total_active_file" : 0,
+             "active_anon" : 6537216,
+             "total_active_anon" : 6537216,
+             "total_pgpgout" : 414,
+             "total_cache" : 0,
+             "inactive_anon" : 0,
+             "active_file" : 0,
+             "pgfault" : 964,
+             "inactive_file" : 0,
+             "total_pgpgin" : 477
+          },
+          "max_usage" : 6651904,
+          "usage" : 6537216,
+          "failcnt" : 0,
+          "limit" : 67108864
+       },
+       "blkio_stats": {
+          "io_service_bytes_recursive": [
+             {
+                "major": 8,
+                "minor": 0,
+                "op": "Read",
+                "value": 428795731968
+             },
+             {
+                "major": 8,
+                "minor": 0,
+                "op": "Write",
+                "value": 388177920
+             }
+          ],
+          "io_serviced_recursive": [
+             {
+                "major": 8,
+                "minor": 0,
+                "op": "Read",
+                "value": 25994442
+             },
+             {
+                "major": 8,
+                "minor": 0,
+                "op": "Write",
+                "value": 1734
+             }
+          ],
+          "io_queue_recursive": [],
+          "io_service_time_recursive": [],
+          "io_wait_time_recursive": [],
+          "io_merged_recursive": [],
+          "io_time_recursive": [],
+          "sectors_recursive": []
+       },
+       "cpu_stats" : {
+          "cpu_usage" : {
+             "percpu_usage" : [
+                16970827,
+                1839451,
+                7107380,
+                10571290
+             ],
+             "usage_in_usermode" : 10000000,
+             "total_usage" : 36488948,
+             "usage_in_kernelmode" : 20000000
+          },
+          "system_cpu_usage" : 20091722000000000
+       }
+    }`
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(jsonStats))
+	}))
+	DockerEndpoint = server.URL
+	defer server.Close()
+	var cont docker.Container
+	stats, err := getMetricFromContainer(&cont)
+	expected := map[string]string{
+		"mem_pct_max": "9.74",
+		"cpu_max":     "0.00",
+		"mem_max":     "6537216",
+	}
+	c.Assert(stats, check.DeepEquals, expected)
+	c.Assert(err, check.IsNil)
 }
