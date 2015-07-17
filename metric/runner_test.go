8d464fe733b91e0902e4c5315baf30996e91e0ce
Cezar Sa Espinola <cezarsa@gmail.com>
2015-07-17 17:53:31 -0300

    metric: runner test checking if metrics are sent

--- metric/runner_test.go
+++ metric/runner_test.go
@@ -21,23 +21,46 @@ type bogusContainer struct {
 
 func (s *S) TestRunner(c *check.C) {
 	bogusContainers := []bogusContainer{
-		{config: docker.Config{Image: "tsuru/python", Env: []string{"HOME=/", "TSURU_APPNAME=someapp"}}, state: docker.State{Running: true}},
+		{config: docker.Config{Image: "tsuru/python", Env: []string{"HOME=/", "TSURU_APPNAME=someapp", "TSURU_PROCESSNAME=myprocess"}}, state: docker.State{Running: true}},
 		{config: docker.Config{Image: "tsuru/python", Env: []string{"HOME=/", "TSURU_APPNAME=someapp"}}, state: docker.State{Running: false, ExitCode: -1}},
 	}
-	dockerServer, _ := s.startDockerServer(bogusContainers, nil, c)
+	dockerServer, conts := s.startDockerServer(bogusContainers, nil, c)
 	defer dockerServer.Stop()
+	dockerServer.PrepareStats(conts[0].ID, func(id string) docker.Stats {
+		s := docker.Stats{}
+		s.PreCPUStats.CPUUsage.TotalUsage = 50
+		s.PreCPUStats.SystemCPUUsage = 20
+		s.CPUStats.CPUUsage.TotalUsage = 100
+		s.CPUStats.SystemCPUUsage = 40
+		s.CPUStats.CPUUsage.PercpuUsage = []uint64{100}
+		return s
+	})
 	os.Setenv("METRICS_BACKEND", "fake")
 	defer os.Unsetenv("METRICS_BACKEND")
 	r := NewRunner(dockerServer.URL(), time.Second)
 	err := r.Start()
 	c.Assert(err, check.IsNil)
 	r.Stop()
+	var cpuStat *fakeStat
+	for i, stat := range fakeStatter.stats {
+		if stat.key == "cpu_max" {
+			cpuStat = &fakeStatter.stats[i]
+		}
+	}
+	c.Assert(cpuStat, check.NotNil)
+	expected := &fakeStat{
+		app:      "someapp",
+		hostname: conts[0].ID[:12],
+		process:  "myprocess",
+		key:      "cpu_max",
+		value:    "250.00",
+	}
+	c.Assert(cpuStat, check.DeepEquals, expected)
 }
 
 func (s *S) startDockerServer(containers []bogusContainer, hook func(*http.Request), c *check.C) (*testing.DockerServer, []docker.Container) {
 	server, err := testing.NewServer("127.0.0.1:0", nil, hook)
 	c.Assert(err, check.IsNil)
-	// server.CustomHandler("", handler)
 	client, err := docker.NewClient(server.URL())
 	c.Assert(err, check.IsNil)
 	createdContainers := make([]docker.Container, len(containers))
