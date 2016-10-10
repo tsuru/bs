ba5d7a6fdb1d0b83de91eb0eecc1c4ed30c75dbb
Cezar Sa Espinola <cezarsa@gmail.com>
2016-10-10 15:23:19 -0300

    log: use bufio for fewer syslog forwarding writes when using tcp
    Huge performance improvement for syslog forwarding.
    
    benchmark                                     old ns/op     new ns/op     delta
    BenchmarkMessagesWaitOneSyslogAddress-4       10016         1569          -84.34%
    BenchmarkMessagesWaitTwoSyslogAddresses-4     49025         2246          -95.42%
    BenchmarkMessagesBroadcast-4                  5461          5667          +3.77%
    BenchmarkMessagesBroadcastWaitTsuru-4         17954         16951         -5.59%
    
    benchmark                                     old allocs     new allocs     delta
    BenchmarkMessagesWaitOneSyslogAddress-4       4              3              -25.00%
    BenchmarkMessagesWaitTwoSyslogAddresses-4     8              5              -37.50%
    BenchmarkMessagesBroadcast-4                  11             10             -9.09%
    BenchmarkMessagesBroadcastWaitTsuru-4         16             16             +0.00%
    
    benchmark                                     old bytes     new bytes     delta
    BenchmarkMessagesWaitOneSyslogAddress-4       347           84            -75.79%
    BenchmarkMessagesWaitTwoSyslogAddresses-4     710           162           -77.18%
    BenchmarkMessagesBroadcast-4                  638           602           -5.64%
    BenchmarkMessagesBroadcastWaitTsuru-4         793           793           +0.00%

--- log/syslogforwarder.go
+++ log/syslogforwarder.go
@@ -5,6 +5,7 @@
 package log
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"net/url"
@@ -129,10 +130,44 @@ func (b *syslogBackend) stop() {
 	}
 }
 
+type bufferedSyslogConn struct {
+	net.Conn
+	w  *bufio.Writer
+	to time.Duration
+}
+
+func (c *bufferedSyslogConn) Write(msg []byte) (int, error) {
+	if c.to > 0 && len(msg) > c.w.Available() {
+		c.Conn.SetWriteDeadline(time.Now().Add(c.to))
+	}
+	return c.w.Write(msg)
+}
+
+func (c *bufferedSyslogConn) Close() error {
+	if c.to > 0 {
+		c.Conn.SetWriteDeadline(time.Now().Add(c.to))
+	}
+	c.w.Flush()
+	return c.Conn.Close()
+}
+
+func (c *bufferedSyslogConn) SetWriteDeadline(deadline time.Time) error {
+	if deadline.IsZero() {
+		c.to = 0
+		c.Conn.SetWriteDeadline(time.Time{})
+	} else {
+		c.to = deadline.Sub(time.Now())
+	}
+	return nil
+}
+
 func (f *syslogForwarder) connect() (net.Conn, error) {
 	conn, err := net.DialTimeout(f.url.Scheme, f.url.Host, forwardConnDialTimeout)
 	if err != nil {
 		return nil, fmt.Errorf("[log forwarder] unable to connect to %q: %s", f.url, err)
+	}
+	if f.url.Scheme == "tcp" {
+		conn = &bufferedSyslogConn{Conn: conn, w: bufio.NewWriter(conn)}
 	}
 	return conn, nil
 }
